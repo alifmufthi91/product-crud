7a9a088ebe1cf7ac99066dae2457c9a96f923497
Alif Mufthi <alif.stinky@gmail.com>
2021-11-07 21:35:01 +0700

    feat: added migration

--- config/env.js
+++ config/env.js
@@ -1,22 +1,22 @@
-/* eslint-disable indent */
-import dotenv from 'dotenv'
+// import dotenv from 'dotenv'
+const dotenv = require('dotenv')
 
 dotenv.config()
 const env = {
-    PORT: process.env.PORT,
+  PORT: process.env.PORT,
 
-    DB_HOST: process.env.DB_HOST,
-    DB_PORT: process.env.DB_PORT,
-    DB_NAME: process.env.DB_NAME,
-    DB_USERNAME: process.env.DB_USERNAME,
-    DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_NAME: process.env.DB_NAME,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
 
-    JWT_SECRET: process.env.JWT_SECRET,
-    JWT_TTL: process.env.JWT_TTL,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_TTL: process.env.JWT_TTL,
 
-    FILE_STORAGE: process.env.FILE_STORAGE,
-    FILE_MAXIMUM_SIZE: process.env.FILE_MAXIMUM_SIZE
+  FILE_STORAGE: process.env.FILE_STORAGE,
+  FILE_MAXIMUM_SIZE: process.env.FILE_MAXIMUM_SIZE
 
 }
 
-export default env
+module.exports = env
---
+++ migrations/init.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2')
+const fs = require('fs')
+const path = require('path')
+const env = require('../config/env')
+const { exit } = require('process')
+
+const dbConnection = mysql.createConnection({
+  host: env.DB_HOST,
+  port: env.DB_PORT,
+  user: env.DB_USERNAME,
+  password: env.DB_PASSWORD,
+  multipleStatements: true
+})
+const filePath = path.join(__dirname, './01-product-crud.sql')
+const queryFile = fs.readFileSync(filePath).toString()
+
+dbConnection.query(queryFile, function (err, results) {
+  if (err) throw err
+  console.log(results)
+  exit(1)
+})
